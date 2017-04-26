e72599eb82f4137dce77915cdcf4e42966e8981a
Lu-Chi <fra.proxy@gmail.com>
2017-04-26 15:18:39 +0200

    Added functions: update_tld, domain_generate. To be changed later on.

--- debrute.py
+++ debrute.py
@@ -11,6 +11,17 @@ def bruteforce(charset, minlength, maxlength):
         for i in range(minlength, maxlength + 1)))
 
 
+
+def update_tld():
+	''' a function to update the TLD list from https://www.icann.org/resources/pages/tlds-2012-02-25-en '''
+	pass
+
+
+def domain_generate():
+	''' a function to generate domain with provided strings and TLDs. '''
+	pass
+
+
 def main(args):
 	charset = 'abcdefghijklmnopqrstuvwxyz'
 	records = {'a':'A', 'aaaa':'AAAA', 'ns':'NS', 'ptr':'PTR', 'mx':'MX', 'srv':'SRV', 'txt':'TXT', 'soa':'SOA', 'spf':'SPF'}
@@ -52,7 +63,7 @@ if __name__ == '__main__':
 						If no argument given - print all of them.')
 	parser.add_argument('--min', default=0, required=False, help='Minimal string length.')
 	parser.add_argument('--max', default=0, required=False, help='Maximal string length.')
-	parser.add_argument('-t', '--tld', required=False, help='Top Level Domain. Available options - all, known, selected')
+	parser.add_argument('-t', '--tld', required=False, help='Top Level Domain. Available options - update, all, known, selected.')
 	parser.add_argument('-p', '--print', required=False, help='Print only found records.') 	
 	parser.add_argument('-f', '--file', required=False, help='Save output to a file.')
 	args = parser.parse_args()
